3bb27b701e5e7de928459d77104d40dd325dd385
aswithaa22 <127473209+aswithaa22@users.noreply.github.com>
2023-11-06 15:17:57 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,b,sum;
+printf("Enter the first number:\n");
+scanf("%d",&a);
+printf("Enter the second number:\n");
+scanf("%d",&b);
+sum=a+b;
+printf("sum=%d\n",sum);
+}
